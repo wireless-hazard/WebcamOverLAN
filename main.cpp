6c3d9ce1e56bb684abda71918b70c8092e28119e
wireless-hazard <magnocostamaia@gmail.com>
2024-09-17 21:08:31 -0300

    Add the udp stream to the main application

--- main.cpp
+++ main.cpp
@@ -6,22 +6,26 @@ int main (int argc, char *argv[])
     gst_init (&argc, &argv);
 
     /* Create the elements */
-    GstElement *source = gst_element_factory_make ("v4l2src", "source");
-    GstElement *sink = gst_element_factory_make ("autovideosink", "sink");
-    GstElement *video_convert = gst_element_factory_make ("videoconvert", "video_convert");
+    GstElement *source = gst_element_factory_make("v4l2src", nullptr);
+    GstElement *sink = gst_element_factory_make("udpsink", "sink");
+    GstElement *rtpvrawpay_element = gst_element_factory_make("rtpvrawpay", nullptr);
+    GstElement *video_convert = gst_element_factory_make("videoconvert", "video_convert");
+
+    /* Sets the IP:PORT to be used by the UDP socket*/
+    g_object_set(sink, "host", "127.0.0.1", "port", 5200, NULL);
 
     /* Create the empty pipeline */
-    GstElement *pipeline = gst_pipeline_new ("simple-pipeline");
+    GstElement *pipeline = gst_pipeline_new("simple-pipeline");
 
-    if (!pipeline || !source || !sink || !video_convert) {
+    if (!pipeline || !source || !sink || !video_convert || !rtpvrawpay_element) {
         g_printerr ("Not all elements could be created.\n");
         return -1;
     }
 
     /* Build the pipeline */
-    gst_bin_add_many (GST_BIN (pipeline), source, sink, video_convert, nullptr);
+    gst_bin_add_many(GST_BIN (pipeline), source, sink, video_convert, rtpvrawpay_element, nullptr);
 
-    GstCaps *caps = gst_caps_new_simple ("video/x-raw",
+    GstCaps *caps = gst_caps_new_simple("video/x-raw",
         "framerate", GST_TYPE_FRACTION, 30, 1,
         nullptr);
 
@@ -33,30 +37,30 @@ int main (int argc, char *argv[])
     }
 
     /* Link all elements that can be automatically linked because they have "Always" pads */
-    if (gst_element_link_many (video_convert, sink, nullptr) != TRUE) {
-        g_printerr ("Elements could not be linked.\n");
-        gst_object_unref (pipeline);
+    if (gst_element_link_many(video_convert, rtpvrawpay_element, sink, nullptr) != TRUE) {
+        g_printerr("Elements could not be linked.\n");
+        gst_object_unref(pipeline);
         return -1;
     }
 
     /* Start playing */
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
     /* Wait until error or EOS */
-    GstBus *bus = gst_element_get_bus (pipeline);
+    GstBus *bus = gst_element_get_bus(pipeline);
     GstMessage *msg =
-        gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+        gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
             (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
     /* See next tutorial for proper error message handling/parsing */
-    if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
+    if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
         g_printerr ("An error occurred! Re-run with the GST_DEBUG=*:WARN "
         "environment variable set for more details.\n");
     }
 
     /* Free resources */
-    gst_message_unref (msg);
-    gst_object_unref (bus);
-    gst_element_set_state (pipeline, GST_STATE_NULL);
-    gst_object_unref (pipeline);
+    gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
 }
